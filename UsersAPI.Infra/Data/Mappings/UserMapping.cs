eaabab3cbebc413b79c393d2e380fc2b1c6cc34e
Dieimes <tecsysteminformatica@gmail.com>
2023-08-23 17:06:13 -0300

    Add project files.

---
+++ UsersAPI.Infra/Data/Mappings/UserMapping.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.Domain.Models;
+
+namespace UsersAPI.Infra.Data.Mappings
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+
+            builder.HasKey(c => c.Id).HasName("Id");
+
+            builder.Property(c => c.Id).IsRequired();
+            builder.Property(c => c.Login).IsRequired().HasMaxLength(20);
+            builder.Property(c => c.Password).IsRequired().HasMaxLength(30);
+          
+        }
+    }
+}
---
+++ UsersAPI.Infra/Data/UserContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Users.Domain.Models;
+using System.Reflection;
+
+namespace UsersAPI.Infra.Data
+{
+    public partial class UserDBContext : DbContext
+    {
+        public UserDBContext()
+        {
+        }
+
+        public UserDBContext(DbContextOptions<UserDBContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        
+
+       
+    }
+}
